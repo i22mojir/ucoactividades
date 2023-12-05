b4e8128a8475bcf4db1f84ce2914f0d9e35cbb82
Rafael Molleja <i22mojir@uco.es>
2023-12-05 18:21:36 +0100

    changed name of directory src

---
+++ src/classes/mail/mail.h
@@ -0,0 +1,22 @@
+#ifndef EMAIL_H
+#define EMAIL_H
+
+#include <string>
+
+class mail
+{
+private:
+std::string title_;
+
+
+
+public:
+
+
+}
+
+
+
+
+
+#endif
---
+++ src/classes/user/user.h
@@ -0,0 +1,26 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+
+
+class user
+{
+private:
+    std::string user_name_;
+    std::string email_;
+    std::string password_;
+    //enum con intereses
+    bool admin_status_; //0 para usuarios comunes y 1 para Administradores  
+    bool blocked_status_; //0 para usuarios no bloqueados y 1 para usuarios bloqueados
+
+public:
+    std::string GetuserName(){return user_name_;}
+    std::string GetEmail(){return email_;}
+    std::string Getpassword(){return password_;}
+    //void GetIntereses(){}
+    bool GetAdmin(){return admin_status_;}
+    bool GetBlocked(){return blocked_status_;}
+}
+
+#endif
