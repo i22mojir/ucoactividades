e32a85281ef7aef5dbcd01c1a99c6d6a6f32ad22
Laura <i22mujul>
2023-12-11 21:09:32 +0100

    update

--- src/classes/user/user.h
+++ src/classes/user/user.h
@@ -1,7 +1,18 @@
 #ifndef USER_H
 #define USER_H
 
+#include <iostream>
+#include <vector>
 #include <string>
+
+enum Intereses
+{
+    Tecnologia,
+    Medicina,
+    Ciencia,
+    Derecho,
+    Ninguno
+};
 
 class User
 {
@@ -9,26 +20,26 @@ private:
     std::string user_name_;
     std::string email_;
     std::string password_;
-    //enum con intereses
-    bool admin_status_; //0 para usuarios comunes y 1 para Administradores  
-    bool blocked_status_; //0 para usuarios no bloqueados y 1 para usuarios bloqueados
+    Intereses intereses_;
+    bool admin_status_; //0 para usuarios comunes y 1 para Administradores
 
 public:
-    User(std::string user_name="empty", std::string email="empty", std::string password="empty", bool admin_status=0, bool blocked_status=0)
-    {user_name_ = user_name; email_ = email; password_ = password; admin_status_ = admin_status; blocked_status_ = blocked_status;}
+    User(std::string user_name = "empty", std::string email = "empty", std::string password = "empty", Intereses intereses = Ninguno, bool admin_status = false)
+        {user_name_ = user_name; email_ = email; password_ = password; intereses_ = intereses; admin_status_ = admin_status;}
 
-    std::string GetUserName(){return user_name_;}
-    std::string GetEmail(){return email_;}
-    std::string GetPassword(){return password_;}
-    //void GetIntereses(){}
-    bool GetAdmin(){return admin_status_;}
-    bool GetBlocked(){return blocked_status_;}
+    std::string GetUserName() const { return user_name_; }
+    std::string GetEmail() const { return email_; }
+    std::string GetPassword() const { return password_; }
+    Intereses GetIntereses() const { return intereses_; }
+    bool GetAdmin() const { return admin_status_; }
 
-    void SetUserName(std::string user_name){user_name_ = user_name;}
-    void SetEmail(std::string email){email_ = email;}
-    void SetPassword(std::string password){password_ = password;}
-    void SetAdminStatus(bool admin_status){admin_status_ = admin_status;}
-    void SetBlocked(bool blocked_status){blocked_status_ = blocked_status;}
+    void SetUserName(std::string user_name) { user_name_ = user_name; }
+    void SetEmail(std::string email) { email_ = email; }
+    void SetPassword(std::string password) { password_ = password; }
+    void SetIntereses(Intereses intereses) { intereses_ = intereses; }
+    void SetAdminStatus(bool admin_status) { admin_status_ = admin_status; }
+
+    std::string GetDetails() const;
 };
 
 #endif
